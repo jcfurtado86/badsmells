6669cda09944d616eb2283278d73280a4a1de844
Matheus <Matheus@LAPTOP-AH81Q6AB>
2018-08-25 13:33:18 -0300

    Correções

--- src/br/unifap/serde/projectvisualizer/actions/JTreeActions.java
+++ src/br/unifap/serde/projectvisualizer/actions/JTreeActions.java
@@ -10,6 +10,8 @@ import br.unifap.serde.projectvisualizer.util.ButtonTabComponent;
 import br.unifap.serde.projectvisualizer.entities.FileNode;
 import br.unifap.serde.projectvisualizer.ui.OpenProjectGUI;
 import br.unifap.serde.projectvisualizer.ui.TabbedPaneGUI;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -30,12 +32,38 @@ import reconhecedor.Reconhecedor;
  *
  * @author furtado
  */
-public class JTreeActions {
+public class JTreeActions implements KeyListener {
+    
+    private JTree jTree; 
+    private JTabbedPane jTabbedPane;
+            
+    public JTreeActions(JTree jTree, JTabbedPane jTabbedPane){
+        this.jTree = jTree;
+        this.jTabbedPane = jTabbedPane;
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            clickedKey(this.jTree, this.jTabbedPane);
+        }
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {}
 
+    @Override
+    public void keyReleased(KeyEvent e) {}
+        
     public static void jTreeMouseClicked(MouseEvent evt, JTree jTree, JTabbedPane jTabbedPane) {
+        clicked(evt, jTree, jTabbedPane);
+    }
+        
+    public static void clicked(MouseEvent evt, JTree jTree, JTabbedPane jTabbedPane){
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree.getLastSelectedPathComponent();
         JTree jTTemp = (JTree) evt.getComponent();
         if (evt.getClickCount() == 2 && jTTemp.getClosestPathForLocation(evt.getX(), evt.getY()).getLastPathComponent() == node) {
+            
             if (node != null) {
                 FileNode info = (FileNode) node.getUserObject();
                 if (info.getFile().isFile()) {
@@ -71,5 +99,42 @@ public class JTreeActions {
 
         }
     }
+    
+    public static void clickedKey(JTree jTree, JTabbedPane jTabbedPane){
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) jTree.getLastSelectedPathComponent();
+        
+        if (node != null) {
+            FileNode info = (FileNode) node.getUserObject();
+            if (info.getFile().isFile()) {
+                int opened = jTabbedPane.indexOfTab(node.toString());
+                if (opened == -1) {
+                    JPanel newTab = TabbedPaneGUI.createTab();
+                    jTabbedPane.add(node.toString(), newTab);
+                    jTabbedPane.setTabComponentAt(jTabbedPane.getTabCount() - 1, new ButtonTabComponent(jTabbedPane));
+                    jTabbedPane.setSelectedComponent(newTab);
+                    try {
+                        BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(info.getFile())));
+                        JScrollPane tempJSCP = (JScrollPane) newTab.getComponent(0);
+                        JTextArea tempJTA = (JTextArea) tempJSCP.getViewport().getComponent(0);
+                        tempJTA.read(input, info.getFile().getName());
 
+                        Reconhecedor find = new Reconhecedor();
+                        ArrayList<String> resultado = find.executar(tempJTA.getText());                           
+                        String resultadoSaida = "";
+                        for(int i=0;i<resultado.size();i++){
+                            resultadoSaida = resultadoSaida + resultado.get(i);
+                        }
+
+                        tempJTA.setText(resultadoSaida);
+
+                    } catch (IOException ex) {
+                        Logger.getLogger(OpenProjectGUI.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    jTabbedPane.setSelectedIndex(opened);
+                }
+            }
+        }
+    }
+    
 }
